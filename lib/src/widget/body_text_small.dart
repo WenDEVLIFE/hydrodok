f27bdf4dbb2356994669ab872ed0cec4ad7bb835
WenDEVLIFE <frouenmedinajr@gmail.com>
2026-07-19 08:49:44 +0800

    aded body_text_small.dart

---
+++ lib/src/widget/body_text_small.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/widgets.dart';
+import '../core/utils/typography.dart';
+
+class BodyTextSmall extends StatelessWidget {
+  final String text;
+  final Color? color;
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+  final FontWeight? fontWeight;
+  final double? fontSize;
+
+  const BodyTextSmall(
+    this.text, {
+    super.key,
+    this.color,
+    this.textAlign,
+    this.maxLines,
+    this.overflow,
+    this.fontWeight,
+    this.fontSize,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: AppTypography.bodySmall(color: color).copyWith(
+        fontWeight: fontWeight,
+        fontSize: fontSize,
+      ),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      overflow: overflow,
+    );
+  }
+}
